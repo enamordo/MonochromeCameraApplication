213fa01d95c6f394e6fd0e2cf3226524c8afb68b
Atsunobu Yuge <yugechan666@gmail.com>
2024-05-14 23:45:17 +0900

    Initial Commit

---
+++ MonochromeCameraApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MonochromeCameraApplication
+//
+//  Created by 弓削　敦信 on 2024/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
