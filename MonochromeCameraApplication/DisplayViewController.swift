0e63e9299f8bd399742810e38263d8ab27481ea1
Atsunobu Yuge <yugechan666@gmail.com>
2024-05-18 18:12:31 +0900

    #15 画面2に「終了ボタン」作成（押下すると、画面1に戻る）

--- MonochromeCameraApplication/DisplayViewController.swift
+++ MonochromeCameraApplication/DisplayViewController.swift
@@ -15,15 +15,17 @@ class DisplayViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeModal(_ sender: UIButton) {
+        /*
+        MARK: UINavigationControllerを利用して戻るやり方
+        pushViewController()メソッド、あるいはSegueのShowで遷移してこないと、
+        popViewController()やpopToRootViewController()が利用できないらしい。
+        不本意だが、元の画面から遷移したのと同じ方法で戻る。
+         */
+        let vc = self.storyboard?.instantiateViewController(identifier: "Initial") as! ViewController
+        let uinc = UINavigationController(rootViewController: vc)
+        // スワイプで戻れてしまうプッシュ遷移でなく、モーダル遷移のフルスクリーンにする
+        uinc.modalPresentationStyle = .fullScreen
+        self.navigationController?.present(uinc, animated: true)
     }
-    */
-
 }
