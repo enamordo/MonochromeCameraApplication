f29c711caed84c9830d84aa45362cbfbfe63c1e4
Atsunobu Yuge <yugechan666@gmail.com>
2024-05-18 18:25:29 +0900

    #15 画面1と同じやり方で遷移していたのを、dismiss利用に修正。

--- MonochromeCameraApplication/DisplayViewController.swift
+++ MonochromeCameraApplication/DisplayViewController.swift
@@ -11,21 +11,10 @@ class DisplayViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func closeModal(_ sender: UIButton) {
-        /*
-        MARK: UINavigationControllerを利用して戻るやり方
-        pushViewController()メソッド、あるいはSegueのShowで遷移してこないと、
-        popViewController()やpopToRootViewController()が利用できないらしい。
-        不本意だが、元の画面から遷移したのと同じ方法で戻る。
-         */
-        let vc = self.storyboard?.instantiateViewController(identifier: "Initial") as! ViewController
-        let uinc = UINavigationController(rootViewController: vc)
-        // スワイプで戻れてしまうプッシュ遷移でなく、モーダル遷移のフルスクリーンにする
-        uinc.modalPresentationStyle = .fullScreen
-        self.navigationController?.present(uinc, animated: true)
+    @IBAction func dismiss(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
 }
